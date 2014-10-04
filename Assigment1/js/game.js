570e286c95e9a01d16046a3d518ffd91ee6c45e6
Carolyn Marshall <carolynmarshall@gmail.com>
2014-10-04 14:28:33 -0400

    remove commented examples

--- Assigment1/js/game.js
+++ Assigment1/js/game.js
@@ -174,17 +174,7 @@ var main = function () {
 	// Request to do this again ASAP
 	requestAnimationFrame(main);
 };
-/*
-function getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-      }
-      var canvas = document.getElementById('myCanvas');
-      var context = canvas.getContext('2d');
-*/
+
 var dragHero = false;
 var mouseDown = false;
 var mousePosX = 0;
@@ -208,15 +198,6 @@ canvas.addEventListener('mouseup', function(event) {
   dragHero = false;
 });
 
-/*
-      addEventListener('mousemove', function(evt) {
-        var mousePos = getMousePos(canvas, evt);
-        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-        writeMessage(canvas, message);
-      }, false);
-
-canvas.addEventListener('mou')*/
-// Cross-browser support for requestAnimationFrame
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
